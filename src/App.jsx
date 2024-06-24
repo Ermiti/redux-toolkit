50828758fbae307c70b6c503423d881d3d65b8d4
Ermiti <k50840725@gmail.com>
2024-06-24 15:37:51 +0330

    commited

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Task from './task';
+import TaskList from './taskList';
+
+function App() {
+  return (
+<div className="App">
+      <Task />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
